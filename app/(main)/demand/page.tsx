62ff7fa548d88c9ee9fd73cc1b6a2d262c577a03
Rasel Miah <rasel6041@gmail.com>
2025-10-02 13:25:15 +0100

    demand filter

--- app/(main)/demand/page.tsx
+++ app/(main)/demand/page.tsx
@@ -1,12 +1,16 @@
+import CreateDemandForm from "./DemandForm";
+
 export const metadata = { title: "Demand • Bilvio" };
 
 export default function DemandPage() {
   return (
-    <section className="max-w-7xl bg-white border shadow-sm p-6 m-4 sm:mx-6 md:mx-8 lg:mx-auto">
-      <header>
-        <h1 className="text-2xl md:text-3xl font-extrabold tracking-tight">Demand</h1>
-      </header>
-      <p className="text-sm text-muted-foreground mt-2">Start building your demand tools here.</p>
-    </section>
+    <div className="max-w-7xl mx-auto w-full">
+      <h1 className="text-2xl md:text-3xl font-extrabold mt-4">Demands</h1>
+      <div className="mx-2 sm:mx-4 md:mx-6">
+      <div className="bg-white border shadow-sm p-4 mt-4 sm:mx-6 md:mx-8 lg:mx-auto">
+        <CreateDemandForm />
+      </div>
+      </div>
+   </div>
   );
 }
