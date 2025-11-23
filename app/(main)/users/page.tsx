0778e5ad9c79f3c5adbd8f8948a7de90ba4fdbf3
raselrzl <rasel6041@gmail.com>
2025-11-23 17:27:28 +0100

    product note for all product

--- app/(main)/users/page.tsx
+++ app/(main)/users/page.tsx
@@ -23,7 +23,7 @@ export default async function UsersPage() {
     <div className="max-w-7xl mx-auto w-full">
       <div className="flex items-center justify-between px-2 2xl:px-2 mt-6">
         <h1 className="text-2xl md:text-3xl font-extrabold">Users</h1>
-        <Link href="/admin/users/create">
+        <Link href="/users/create">
           <Button className="rounded-xs inline-flex items-center gap-2 bg-amber-600 hover:bg-amber-500 text-white cursor-pointer">
             <PlusCircle className="h-5 w-5" aria-hidden="true" />
             <span>Create user</span>
